f59b4239280eec10ea7525336e1067963a1d1146
Travis CI User <travis@example.org>
2017-12-12 02:33:19 +0000

    Updates

---
+++ component---src-pages-index-tsx-e2d8a5815871b968c214.js
@@ -0,0 +1,16 @@
+webpackJsonp([221374088121123],{
+
+/***/ 180:
+/***/ (function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", { value: true });
+	const React = __webpack_require__(4);
+	const IndexPage = () => React.createElement("div", null, React.createElement("p", null, "Welcome to new site."));
+	exports.default = IndexPage;
+
+/***/ })
+
+});
+//# sourceMappingURL=component---src-pages-index-tsx-e2d8a5815871b968c214.js.map
