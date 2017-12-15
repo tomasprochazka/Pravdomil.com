f7a0780125cea7c236f2bddd55b426f4e40527d1
pravdomil <pravdomil.toman@gmail.com>
2017-12-15 18:40:35 +0100

    indent

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,10 +2,10 @@ import * as React from "react";
 import { StatelessComponent } from "react";
 
 const NotFoundPage: StatelessComponent = () => (
-  <div>
-    <h1>Not found</h1>
-    <p>Sorry.</p>
-  </div>
+    <div>
+        <h1>Not found</h1>
+        <p>Sorry.</p>
+    </div>
 );
 
 export default NotFoundPage;
