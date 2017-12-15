7f05063a73bc5822c24a1fe802c8312b68479d75
pravdomil <pravdomil.toman@gmail.com>
2017-12-15 20:43:40 +0100

    Header

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,10 +2,36 @@ import * as React from "react";
 import { StatelessComponent } from "react";
 import "../style.less";
 import { Helmet } from "react-helmet";
+import Spacer from "../components/Spacer";
 
 const Header: StatelessComponent = () => (
     <header>
-        <Link to="/">Pravdomil.com</Link>
+        <div>
+            <Spacer height={8}/>
+            
+            <h2>Pravdomil</h2>
+            <p>personal site</p>
+            
+            <Spacer height={3}/>
+            
+            <div className="links">
+                <a href="mailto:info@pravdomil.com">
+                    <i className="fa fa-envelope"/>
+                </a>
+                <a href="https://twitter.com/pravdomil">
+                    <i className="fa fa-twitter"/>
+                </a>
+                <a href="https://github.com/pravdomil">
+                    <i className="fa fa-github"/>
+                </a>
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BCL2X3AFQBAP2&item_name=donation">
+                    <i className="fa fa-paypal"/>
+                </a>
+                <a href="https://youtube.com/pravdomil">
+                    <i className="fa fa-youtube-play"/>
+                </a>
+            </div>
+        </div>
     </header>
 );
 
