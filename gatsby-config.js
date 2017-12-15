f49b7235c30c40aeadba7cc7076e285939961a0c
pravdomil <pravdomil.toman@gmail.com>
2017-12-15 20:39:41 +0100

    gatsby-source-github-api

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,13 @@ module.exports = {
         "gatsby-plugin-typescript",
         "gatsby-plugin-react-helmet",
         "gatsby-plugin-less",
+        {
+            resolve: "gatsby-source-github-api",
+            options: {
+                token: process.env.GITHUB_TOKEN,
+                graphQLQuery: require("./src/helpers/query.js"),
+                variables: {}
+            }
+        },
     ],
 };
