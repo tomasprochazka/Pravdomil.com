072e808417647413e64914089c9ad9516e239e84
pravdomil <pravdomil.toman@gmail.com>
2017-12-15 20:41:12 +0100

    index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,49 @@
 import * as React from "react";
 import { StatelessComponent } from "react";
+import RepoList from "../components/RepoList";
 
-const IndexPage: StatelessComponent = () => (
-  <div>
-    <p>Welcome to new site.</p>
-  </div>
-);
+const IndexPage: StatelessComponent<{ data: any }> = function({ data }) {
+    const repos = data.githubData.data.viewer.repositories.nodes;
+    
+    return (
+        <div>
+            <RepoList repos={repos}/>
+        </div>
+    )
+};
+
+declare const graphql: any;
+export const query = graphql`
+    query Repos {
+        githubData {
+            data {
+                viewer {
+                    repositories {
+                        nodes {
+                            name
+                            shortDescriptionHTML
+
+                            url
+                            homepageUrl
+
+                            viewerHasStarred
+                            stargazers {
+                                totalCount
+                            }
+
+                            repositoryTopics {
+                                nodes {
+                                    topic {
+                                        name
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
 
 export default IndexPage;
