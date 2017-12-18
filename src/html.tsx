600d22750f89d7e6db5b181e4b6f589c24af054e
pravdomil <pravdomil.toman@gmail.com>
2017-12-18 19:48:23 +0100

    inlined-css

--- src/html.tsx
+++ src/html.tsx
@@ -1,11 +1,21 @@
 import * as React from "react";
 import { StatelessComponent } from "react";
 
+let stylesStr;
+if (process.env.NODE_ENV === "production") {
+    try {
+        stylesStr = require("!raw-loader!../public/styles.css");
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 const HTML: StatelessComponent<any> = (props) => {
     return (
         <html {...props.htmlAttributes}>
         <head>
             <meta charSet="utf-8"/>
+            <style id="gatsby-inlined-css" dangerouslySetInnerHTML={{ __html: stylesStr }}/>
             {props.headComponents}
         </head>
         <body {...props.bodyAttributes}>
