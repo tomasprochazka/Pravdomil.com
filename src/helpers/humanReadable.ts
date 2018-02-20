c00c31b52a889251502c5b8c60344e462e3aca78
Pravdomil <pravdomil.toman@gmail.com>
2018-02-20 12:46:28 +0100

    opt

--- src/helpers/humanReadable.ts
+++ src/helpers/humanReadable.ts
@@ -1,4 +1,4 @@
 export function humanReadable(input: string) {
-    const output = input.replace("wp-", "").replace(/-/g, " ");
+    const output = input.replace(/^wp-/, "").replace(/-/g, " ");
     return output[0].toLocaleUpperCase() + output.substr(1);
 }
