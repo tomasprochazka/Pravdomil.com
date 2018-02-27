48f8d59d7a0d4247b1a96ae4ad74453679aee5be
pravdomil <pravdomil.toman@gmail.com>
2018-02-27 22:36:59 +0100

    no semi

--- bin/readmegen.js
+++ bin/readmegen.js
@@ -1,35 +1,35 @@
-const Glob = require("glob");
+const Glob = require("glob")
 
-main();
+main()
 
 async function main() {
-    const links = await editLinks();
+    const links = await editLinks()
     const readme = `<h1 align="center">Pravdomil.com</h1>
 
 ## Files to edit
 ${links}
-`;
+`
     
-    console.log(readme);
+    console.log(readme)
 }
 
 async function editLinks() {
-    const editLink = "https://github.com/pravdomil/pravdomil.com/edit/master/";
+    const editLink = "https://github.com/pravdomil/pravdomil.com/edit/master/"
     
-    const files = await glob("src/**", { nodir: true });
+    const files = await glob("src/**", { nodir: true })
     const links = files.map(f => {
-        const link = editLink + f;
-        const name = f.replace("src/", "");
-        return `- [${name}](${link})`;
-    });
+        const link = editLink + f
+        const name = f.replace("src/", "")
+        return `- [${name}](${link})`
+    })
     
-    return links.join("\n");
+    return links.join("\n")
 }
 
 function glob(pattern, options) {
     return new Promise((resolve, reject) => {
-        const g = new Glob(pattern, options);
-        g.once("end", resolve);
-        g.once("error", reject);
-    });
+        const g = new Glob(pattern, options)
+        g.once("end", resolve)
+        g.once("error", reject)
+    })
 }
