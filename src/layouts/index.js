19db3df85e60c0c4f078b4b56580f80fdfc5be8a
pravdomil <pravdomil.toman@gmail.com>
2018-02-27 22:26:41 +0100

    reorder

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,76 +3,77 @@ import { Helmet } from "react-helmet";
 import Spacer from "../components/Spacer";
 import "../style.less";
 
-function Header() {
+export default function Layout({ children }) {
     return (
-    <header>
-        <div>
-            <Spacer height={9}/>
+        <main>
+            <Helmet title="Pravdomil.com"/>
+            <Helmet meta={[{ name: "viewport", content: "width=device-width" }]}/>
+            <Helmet script={[{ src: require("file!rembased/js/rembased_class") }]}/>
             
-            <h2>Pravdomil</h2>
-            <p>
-                <span>
-                    all my projects on one page
-                </span>
-                <br/>
-                <a href="https://github.com/search?q=user%3Apravdomil&s=stars&type=Repositories">
-                    <small>sorted by github stars</small>
-                </a>
-            </p>
-            
-            <Spacer height={3}/>
-            
-            <div className="links">
-                <a href="mailto:info@pravdomil.com">
-                    <i className="fa fa-envelope"/>
-                </a>
-                <a href="https://twitter.com/pravdomil">
-                    <i className="fa fa-twitter"/>
-                </a>
-                <a href="https://github.com/pravdomil">
-                    <i className="fa fa-github"/>
-                </a>
-                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BCL2X3AFQBAP2&item_name=donation">
-                    <i className="fa fa-paypal"/>
-                </a>
-                <a href="https://youtube.com/pravdomil">
-                    <i className="fa fa-youtube-play"/>
-                </a>
+            <Header/>
+            <div>
+                {children()}
             </div>
-        </div>
-    </header>
+            <Footer/>
+        </main>
     );
 }
 
-export default function Layout({ children }) {
+function Header() {
     return (
-    <main>
-        <Helmet title="Pravdomil.com"/>
-        <Helmet meta={[{ name: "viewport", content: "width=device-width" }]}/>
-        <Helmet script={[{ src: require("file!rembased/js/rembased_class") }]}/>
-        
-        <Header/>
-        <div>
-            {children()}
-        </div>
-        <Footer/>
-    </main>
+        <header>
+            <div>
+                <Spacer height={9}/>
+                
+                <h2>Pravdomil</h2>
+                <p>
+                <span>
+                    all my projects on one page
+                </span>
+                    <br/>
+                    <a href="https://github.com/search?q=user%3Apravdomil&s=stars&type=Repositories">
+                        <small>sorted by github stars</small>
+                    </a>
+                </p>
+                
+                <Spacer height={3}/>
+                
+                <div className="links">
+                    <a href="mailto:info@pravdomil.com">
+                        <i className="fa fa-envelope"/>
+                    </a>
+                    <a href="https://twitter.com/pravdomil">
+                        <i className="fa fa-twitter"/>
+                    </a>
+                    <a href="https://github.com/pravdomil">
+                        <i className="fa fa-github"/>
+                    </a>
+                    <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BCL2X3AFQBAP2&item_name=donation">
+                        <i className="fa fa-paypal"/>
+                    </a>
+                    <a href="https://youtube.com/pravdomil">
+                        <i className="fa fa-youtube-play"/>
+                    </a>
+                </div>
+            </div>
+        </header>
     );
+    
 }
 
 function Footer({}) {
     return (
-    <footer>
-        <Spacer height={16}/>
-        <div>
-            <div className="links">
-                <a href="https://github.com/pravdomil/pravdomil.com#readme">Edit this page</a>
-                <a href="https://gatsbyjs.org">Made with Gatsby</a>
-                <a href="https://travis-ci.org/pravdomil/pravdomil.com">Build by Travis</a>
-                <a href="https://cloudflare.com">Cached by CloudFlare</a>
+        <footer>
+            <Spacer height={16}/>
+            <div>
+                <div className="links">
+                    <a href="https://github.com/pravdomil/pravdomil.com#readme">Edit this page</a>
+                    <a href="https://gatsbyjs.org">Made with Gatsby</a>
+                    <a href="https://travis-ci.org/pravdomil/pravdomil.com">Build by Travis</a>
+                    <a href="https://cloudflare.com">Cached by CloudFlare</a>
+                </div>
             </div>
-        </div>
-        <Spacer height={1}/>
-    </footer>
+            <Spacer height={1}/>
+        </footer>
     );
 }
