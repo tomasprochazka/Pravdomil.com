8eb5de3c628807dfd39344130bd84edbde90de36
pravdomil <info@pravdomil.com>
2018-04-20 13:46:01 +0200

    indent

--- src/helpers/query.js
+++ src/helpers/query.js
@@ -1,28 +1,28 @@
 module.exports = `
 query {
-    viewer {
-        repositories(isFork: false, privacy: PUBLIC, first: 100) {
-            nodes {
-                name
-                description
-                
-                url
-                homepageUrl
-                
-                viewerHasStarred
-                stargazers {
-                    totalCount
-                }
-                
-                repositoryTopics(first: 100) {
-                    nodes {
-                        topic {
-                            name
-                        }
-                    }
-                }
-            }
+  viewer {
+    repositories(isFork: false, privacy: PUBLIC, first: 100) {
+      nodes {
+        name
+        description
+        
+        url
+        homepageUrl
+        
+        viewerHasStarred
+        stargazers {
+          totalCount
         }
+        
+        repositoryTopics(first: 100) {
+          nodes {
+            topic {
+              name
+            }
+          }
+        }
+      }
     }
+  }
 }
 `
