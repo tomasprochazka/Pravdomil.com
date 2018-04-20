6aa33c9b88835ae983274fc74f45e020cc2881e7
pravdomil <info@pravdomil.com>
2018-04-20 13:38:13 +0200

    indent

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    plugins: [
-        "gatsby-plugin-react-helmet",
-        "gatsby-plugin-less",
-        {
-            resolve: "gatsby-source-github-api",
-            options: {
-                token: process.env.GITHUB_TOKEN,
-                graphQLQuery: require("./src/helpers/query.js"),
-                variables: {}
-            }
-        },
-    ],
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-less",
+    {
+      resolve: "gatsby-source-github-api",
+      options: {
+        token: process.env.GITHUB_TOKEN,
+        graphQLQuery: require("./src/helpers/query.js"),
+        variables: {}
+      }
+    },
+  ],
 }
