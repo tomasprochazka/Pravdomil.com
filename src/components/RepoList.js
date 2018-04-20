28e3bbac4aedac4730c93e887bb420553b9084b3
pravdomil <info@pravdomil.com>
2018-04-20 13:35:47 +0200

    indent

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -4,61 +4,61 @@ import { humanReadable } from "../helpers/humanReadable"
 import Spacer from "./Spacer"
 
 function getTopic(topics, name) {
-    const found = topics.find(t => t.name === name)
-    if (found) {
-        return found
-    }
-    else {
-        const stars = name === "uncategorized" ? -100 : 0
-        const topic = { name, repos: [], stars }
-        topics.push(topic)
-        return topic
-    }
+  const found = topics.find(t => t.name === name)
+  if (found) {
+    return found
+  }
+  else {
+    const stars = name === "uncategorized" ? -100 : 0
+    const topic = { name, repos: [], stars }
+    topics.push(topic)
+    return topic
+  }
 }
 
 
 export default function RepoList({ repos }) {
-    // our main topic collection
-    const topics = [];
-    
-    // add repos to corresponding topics
-    [...repos, ...additionalRepos].forEach((repo) => {
-        const repoTopics = repo.repositoryTopics.nodes
-        const topicName = repoTopics.length ? repoTopics[0].topic.name : "uncategorized"
-        
-        const topic = getTopic(topics, topicName)
-        topic.stars += repo.stargazers.totalCount
-        topic.repos.push(repo)
-    })
-    
-    // sort topic repos by stars
-    topics.forEach(topic => {
-        topic.repos.sort((a, b) => a.stargazers.totalCount < b.stargazers.totalCount ? 1 : -1)
-    })
-    
-    // sort topics by stars sum
-    topics.sort((a, b) => a.stars < b.stars ? 1 : -1)
-    
-    return (
-        <div className="repos">
-            {topics.map((topic, i) => (
-                <div className="repo" key={i} id={topic.name}>
-                    <Spacer height={6}/>
-                    <h1 title={topic.stars + " ★"}>{humanReadable(topic.name)}</h1>
-                    <Spacer height={2}/>
-                    <ul>
-                        {topic.repos.map(({ homepageUrl, url, name, description }, c) => (
-                            <li key={c}>
-                                <a href={homepageUrl && !homepageUrl.includes("://pravdomil.com") ? homepageUrl : url + "#readme"} target="_blank">
-                                    <span className="title">{humanReadable(name)}</span>
-                                    <Spacer height={.5}/>
-                                    <span className="desc">{description}</span>
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+  // our main topic collection
+  const topics = [];
+
+  // add repos to corresponding topics
+  [...repos, ...additionalRepos].forEach((repo) => {
+    const repoTopics = repo.repositoryTopics.nodes
+    const topicName = repoTopics.length ? repoTopics[0].topic.name : "uncategorized"
+
+    const topic = getTopic(topics, topicName)
+    topic.stars += repo.stargazers.totalCount
+    topic.repos.push(repo)
+  })
+
+  // sort topic repos by stars
+  topics.forEach(topic => {
+    topic.repos.sort((a, b) => a.stargazers.totalCount < b.stargazers.totalCount ? 1 : -1)
+  })
+
+  // sort topics by stars sum
+  topics.sort((a, b) => a.stars < b.stars ? 1 : -1)
+
+  return (
+    <div className="repos">
+      {topics.map((topic, i) => (
+        <div className="repo" key={i} id={topic.name}>
+          <Spacer height={6} />
+          <h1 title={topic.stars + " ★"}>{humanReadable(topic.name)}</h1>
+          <Spacer height={2} />
+          <ul>
+            {topic.repos.map(({ homepageUrl, url, name, description }, c) => (
+              <li key={c}>
+                <a href={homepageUrl && !homepageUrl.includes("://pravdomil.com") ? homepageUrl : url + "#readme"} target="_blank">
+                  <span className="title">{humanReadable(name)}</span>
+                  <Spacer height={.5} />
+                  <span className="desc">{description}</span>
+                </a>
+              </li>
             ))}
+          </ul>
         </div>
-    )
+      ))}
+    </div>
+  )
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,76 +4,76 @@ import Spacer from "../components/Spacer"
 import "../style.less"
 
 export default function Layout({ children }) {
-    return (
-        <main>
-            <Helmet title="Pravdomil.com"/>
-            <Helmet meta={[{ name: "viewport", content: "width=device-width" }]}/>
-            <Helmet script={[{ src: require("file!rembased/js/rembased_class") }]}/>
-            
-            <Header/>
-            <div>
-                {children()}
-            </div>
-            <Footer/>
-        </main>
-    )
+  return (
+    <main>
+      <Helmet title="Pravdomil.com" />
+      <Helmet meta={[{ name: "viewport", content: "width=device-width" }]} />
+      <Helmet script={[{ src: require("file!rembased/js/rembased_class") }]} />
+
+      <Header />
+      <div>
+        {children()}
+      </div>
+      <Footer />
+    </main>
+  )
 }
 
 function Header() {
-    return (
-        <header>
-            <div>
-                <Spacer height={9}/>
-                
-                <h2><a href="/">Pravdomil</a></h2>
-                <p>
+  return (
+    <header>
+      <div>
+        <Spacer height={9} />
+
+        <h2><a href="/">Pravdomil</a></h2>
+        <p>
                 <span>
                     all my projects on one page
                 </span>
-                    <br/>
-                    <a href="https://github.com/search?q=user%3Apravdomil&s=stars&type=Repositories">
-                        <small>sorted by github stars</small>
-                    </a>
-                </p>
-                
-                <Spacer height={3}/>
-                
-                <div className="links">
-                    <a href="mailto:info@pravdomil.com">
-                        <i className="fa fa-envelope"/>
-                    </a>
-                    <a href="https://twitter.com/pravdomil">
-                        <i className="fa fa-twitter"/>
-                    </a>
-                    <a href="https://github.com/pravdomil">
-                        <i className="fa fa-github"/>
-                    </a>
-                    <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BCL2X3AFQBAP2&item_name=Donation%20pravdomil.com">
-                        <i className="fa fa-paypal"/>
-                    </a>
-                    <a href="https://youtube.com/pravdomil">
-                        <i className="fa fa-youtube-play"/>
-                    </a>
-                </div>
-            </div>
-        </header>
-    )
-    
+          <br />
+          <a href="https://github.com/search?q=user%3Apravdomil&s=stars&type=Repositories">
+            <small>sorted by github stars</small>
+          </a>
+        </p>
+
+        <Spacer height={3} />
+
+        <div className="links">
+          <a href="mailto:info@pravdomil.com">
+            <i className="fa fa-envelope" />
+          </a>
+          <a href="https://twitter.com/pravdomil">
+            <i className="fa fa-twitter" />
+          </a>
+          <a href="https://github.com/pravdomil">
+            <i className="fa fa-github" />
+          </a>
+          <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BCL2X3AFQBAP2&item_name=Donation%20pravdomil.com">
+            <i className="fa fa-paypal" />
+          </a>
+          <a href="https://youtube.com/pravdomil">
+            <i className="fa fa-youtube-play" />
+          </a>
+        </div>
+      </div>
+    </header>
+  )
+
 }
 
 function Footer() {
-    return (
-        <footer>
-            <Spacer height={16}/>
-            <div>
-                <div className="links">
-                    <a href="https://github.com/pravdomil/pravdomil.com#readme">Edit this page</a>
-                    <a href="https://gatsbyjs.org">Made with Gatsby</a>
-                    <a href="https://travis-ci.org/pravdomil/pravdomil.com">Build by Travis</a>
-                    <a href="https://cloudflare.com">Cached by CloudFlare</a>
-                </div>
-            </div>
-            <Spacer height={1}/>
-        </footer>
-    )
+  return (
+    <footer>
+      <Spacer height={16} />
+      <div>
+        <div className="links">
+          <a href="https://github.com/pravdomil/pravdomil.com#readme">Edit this page</a>
+          <a href="https://gatsbyjs.org">Made with Gatsby</a>
+          <a href="https://travis-ci.org/pravdomil/pravdomil.com">Build by Travis</a>
+          <a href="https://cloudflare.com">Cached by CloudFlare</a>
+        </div>
+      </div>
+      <Spacer height={1} />
+    </footer>
+  )
 }
