a233fbafeed2ad25ea0e4cc26833be8d8eb9dfc1
pravdomil <pravdomil.toman@gmail.com>
2018-07-07 10:33:54 +0200

    opt

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from "react"
 import RepoList from "../components/RepoList"
 
-export default function Index({ data }) {
-  if (!(data instanceof Object)) {
+export default function({ data }) {
+  try {
+    const repos = data.githubData.data.viewer.repositories.nodes
+    return <RepoList repos={repos} />
+  } catch (e) {
     throw new Error("No data from GitHub")
   }
-  const repos = data.githubData.data.viewer.repositories.nodes
-
-  return <RepoList repos={repos} />
 }
 
 export const query = graphql`
