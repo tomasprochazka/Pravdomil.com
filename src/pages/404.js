3d4ae390c4ffe006c2b7209b0199e39bd7854c54
pravdomil <pravdomil.toman@gmail.com>
2018-07-07 10:30:51 +0200

    opt

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,8 @@
 import React from "react"
 
-export default function NotFoundPage() {
-  return (
-    <div>
-      <h1>Not found</h1>
-      <p>Sorry.</p>
-    </div>
-  )
-}
+export default () => (
+  <div>
+    <h1>Not found</h1>
+    <p>Sorry.</p>
+  </div>
+)
